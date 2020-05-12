779d9ac78d7d137fb18a3d54ba38248f93aff893
Ushitora Anqou <ushitora@anqou.net>
2020-05-12 20:55:01 +0900

    Add benchmark.sh bottleneck

--- benchmark.sh
+++ benchmark.sh
@@ -7,7 +7,7 @@
 # $ SLACK_API_TOKEN="xxxxx" SLACK_CHANNEL="#channel" ./benchmark.sh [-g NGPUS]
 ###
 
-KVSP_VER=15
+KVSP_VER=17
 
 # Download kvsp if not exist
 if [ ! -f "kvsp_v$KVSP_VER/bin/kvsp" ]; then
@@ -15,14 +15,52 @@ if [ ! -f "kvsp_v$KVSP_VER/bin/kvsp" ]; then
     tar zx
 fi
 
-# Prepare Ruby gems
-bundle install || ( echo "Please install bundler. For example: 'gem install bundler'" && false )
+case "$1" in
+    speed )
+        shift
 
-# Run
-bundle exec ruby benchmark.rb --kvsp-ver $KVSP_VER --superscalar --cmux-memory "$@"
-bundle exec ruby benchmark.rb --kvsp-ver $KVSP_VER --cmux-memory "$@"
-bundle exec ruby benchmark.rb --kvsp-ver $KVSP_VER "$@"
-bundle exec ruby benchmark.rb --kvsp-ver $KVSP_VER --superscalar "$@"
+        # Prepare Ruby gems
+        bundle install || ( echo "Please install bundler. For example: 'gem install bundler'" && false )
 
-# Cleanup
-rm _*
+        # Run
+        bundle exec ruby benchmark.rb --kvsp-ver $KVSP_VER --superscalar --cmux-memory "$@"
+        bundle exec ruby benchmark.rb --kvsp-ver $KVSP_VER --cmux-memory "$@"
+        bundle exec ruby benchmark.rb --kvsp-ver $KVSP_VER "$@"
+        bundle exec ruby benchmark.rb --kvsp-ver $KVSP_VER --superscalar "$@"
+
+        # Cleanup
+        rm _*
+        ;;
+
+    bottleneck )
+        shift
+
+        # Check if faststat is built
+        if [ ! -f faststat/build/faststat ]; then
+            echo "Build faststat in advance"
+            exit 1
+        fi
+
+        # Kill all children at exit
+        # Thanks to: https://stackoverflow.com/a/2173421
+        trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+        # Prepare request packet
+        kvsp_v$KVSP_VER/bin/kvsp cc 03_bf.c -o _elf
+        kvsp_v$KVSP_VER/bin/kvsp genkey -o _sk
+        kvsp_v$KVSP_VER/bin/kvsp enc -k _sk -i _elf -o _req.packet
+
+        # Run faststat
+        faststat_logfile=$(date +'bottleneck-%Y%m%d%H%M%S-faststat.log')
+        faststat/build/faststat > $faststat_logfile &
+
+        # Run kvsp
+        kvsp_logfile=$(date +'bottleneck-%Y%m%d%H%M%S-kvsp.log')
+        kvsp_v$KVSP_VER/bin/kvsp run -c 20 -i _req.packet -o _res.packet -iyokan-args "--stdout-csv" "$@" | tee $kvsp_logfile
+        ;;
+
+    * )
+        echo "Usage: benchmark.sh [speed|bottleneck]"
+        exit 1
+        ;;
+esac
